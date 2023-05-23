9122479f0d16a423d83ca8cc18be8ebd6fb6e361
Yulia Khmyz <xmyza@mail.ru>
2023-05-23 16:00:44 +0300

    course restore

---
+++ server/db/migrations/20230518154650-create-course.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      main_title: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      main_description: {
+        type: Sequelize.TEXT,
+      },
+      start_title: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      start_description: {
+        type: Sequelize.TEXT,
+      },
+      condition_title: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      condition_description: {
+        type: Sequelize.TEXT,
+      },
+      price_title: {
+        type: Sequelize.TEXT,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Courses');
+  },
+};
---
+++ server/db/models/course.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      // Один курс может принадлежать нескольким пользователям
+      this.belongsToMany(User, {
+        foreignKey: 'user_id',
+        through: 'Lessons',
+      });
+    }
+  }
+  Course.init(
+    {
+      main_title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      main_description: {
+        type: DataTypes.TEXT,
+      },
+      start_title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      start_description: {
+        type: DataTypes.TEXT,
+      },
+      condition_title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      condition_description: {
+        type: DataTypes.TEXT,
+      },
+      price_title: {
+        type: DataTypes.TEXT,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Course',
+    }
+  );
+  return Course;
+};
