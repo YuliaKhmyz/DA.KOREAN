1f6a9d3d4b2a8d183630b7c4384dd2b16a0770a9
Yulia Khmyz <xmyza@mail.ru>
2023-05-23 09:54:26 +0300

    problem fix

--- client/src/features/calligraphy/CalligraphyPage.tsx
+++ client/src/features/calligraphy/CalligraphyPage.tsx
@@ -5,7 +5,13 @@ import { useSelector } from 'react-redux';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Container } from 'react-bootstrap';
 import { useAppDispatch } from '../../store';
-import { buyCalligraphy, loadCalligraphies, createCalligraphy, deleteCalligraphy, updateCalligraphy } from './calligraphiesSlice';
+import {
+  buyCalligraphy,
+  loadCalligraphies,
+  createCalligraphy,
+  deleteCalligraphy,
+  updateCalligraphy,
+} from './calligraphiesSlice';
 import { selectCalligraphies } from './selectors';
 import { CalligraphyId, Calligraphy } from './types/Calligraphy';
 import './calligraphyPage.css';
@@ -52,43 +58,59 @@ function CalligraphyPage(): JSX.Element {
         <div>
           <h2>Прописи-антистресс</h2>
           <div>
-            <p>Когда пишешь СЛОВО, поневоле проникаешься смыслом, впитываешь посыл руками, глазами, умом и душой. А смысл в надписях всегда глубокий и мотивирующий. Не думаешь о мелочах, списке продуктов и засохшем цветке. Тело и мозг заземляются, отключаясь от привычной работы.</p>
-            <p>Прописывание букв иностранного алфавита сделает слова “своими”, а язык ближе на уровне тела и визуала. Даже без знания корейского языка можно создать красивую открытку. Процесс подготовки занимает мало времени, но выглядит как обряд инициации перед переходом в другое состояние. Удовольствие от раскладывания кистей, ручек, бумаги и фломастеров обеспечено.</p>
+            <p>
+              Когда пишешь СЛОВО, поневоле проникаешься смыслом, впитываешь
+              посыл руками, глазами, умом и душой. А смысл в надписях всегда
+              глубокий и мотивирующий. Не думаешь о мелочах, списке продуктов и
+              засохшем цветке. Тело и мозг заземляются, отключаясь от привычной
+              работы.
+            </p>
+            <p>
+              Прописывание букв иностранного алфавита сделает слова “своими”, а
+              язык ближе на уровне тела и визуала. Даже без знания корейского
+              языка можно создать красивую открытку. Процесс подготовки занимает
+              мало времени, но выглядит как обряд инициации перед переходом в
+              другое состояние. Удовольствие от раскладывания кистей, ручек,
+              бумаги и фломастеров обеспечено.
+            </p>
           </div>
 
           <h4>Практическая польза от занятий каллиграфией</h4>
-      <div>
-        На выходе получается арт-объект с кучей способов применения. Его можно:
-        <ul>
-          <li>подарить другу, интересующемуся Кореей</li>
-          <li>дополнить любой подарок для уникальности и экзотики </li>
-          <li>повесить на стену </li>
-          <li>отправить по настоящей почте</li>
-          <li>приклеить на мудборд и мотивироваться каждый день</li>
-          <li>составить собственный календарь</li>
-          <li>украсить пенал, эко-сумку или даже майку</li>
-        </ul>
-        <div>
-          <p>
-            После тренировки можно принимать коммерческие заказы на дизайн
-            слоганов, постеров, названий товаров, вывесок магазинов, кафе и
-            ресторанов.
-          </p>
-          <p>
-            В Корее эта отдельная ниша - именно рукописные варианты чаще всего
-            используют на упаковках.
-          </p>
-        </div>
-      </div>
-
-      <div>
-        {calligraphies.map((calligraphy) => (
-          <div key={calligraphy.id}>
-            <CalligraphyItem
-              calligraphy={calligraphy}
-              handleBuyCalligraphy={() => handleBuyCalligraphy(calligraphy.id)}
-            />
+          <div>
+            На выходе получается арт-объект с кучей способов применения. Его
+            можно:
+            <ul>
+              <li>подарить другу, интересующемуся Кореей</li>
+              <li>дополнить любой подарок для уникальности и экзотики </li>
+              <li>повесить на стену </li>
+              <li>отправить по настоящей почте</li>
+              <li>приклеить на мудборд и мотивироваться каждый день</li>
+              <li>составить собственный календарь</li>
+              <li>украсить пенал, эко-сумку или даже майку</li>
+            </ul>
+            <div>
+              <p>
+                После тренировки можно принимать коммерческие заказы на дизайн
+                слоганов, постеров, названий товаров, вывесок магазинов, кафе и
+                ресторанов.
+              </p>
+              <p>
+                В Корее эта отдельная ниша - именно рукописные варианты чаще
+                всего используют на упаковках.
+              </p>
+            </div>
           </div>
+
+          {calligraphies.map((calligraphy) => (
+            <div key={calligraphy.id}>
+              <CalligraphyItem
+                calligraphy={calligraphy}
+                handleBuyCalligraphy={() =>
+                  handleBuyCalligraphy(calligraphy.id)
+                }
+              />
+            </div>
+          ))}
 
           {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
           <form onSubmit={handleSubmit(onSubmit)}>
@@ -96,15 +118,30 @@ function CalligraphyPage(): JSX.Element {
             <label htmlFor="title-input">
               Название прописи
               {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-              <input type="text" placeholder="введите название" id="title-input" {...register('title')} />
+              <input
+                type="text"
+                placeholder="введите название"
+                id="title-input"
+                {...register('title')}
+              />
             </label>
             <label htmlFor="title-input">
               Корейское название
-              <input type="text" placeholder="корейское название" id="koreantitle-input" {...register('koreantitle')} />
+              <input
+                type="text"
+                placeholder="корейское название"
+                id="koreantitle-input"
+                {...register('koreantitle')}
+              />
             </label>
             <label htmlFor="link-input">
               Ссылка скачивания
-              <input type="text" placeholder="введите ссылку" id="link-input" {...register('link')} />
+              <input
+                type="text"
+                placeholder="введите ссылку"
+                id="link-input"
+                {...register('link')}
+              />
             </label>
             <button type="submit">Добавить</button>
           </form>
@@ -113,7 +150,10 @@ function CalligraphyPage(): JSX.Element {
           <div>
             {calligraphies.map((calligraphy) => (
               <div key={calligraphy.id}>
-                <ChangeCalligraphyItem calligraphy={calligraphy} handleDelete={() => handleDelete(calligraphy.id)} />
+                <ChangeCalligraphyItem
+                  calligraphy={calligraphy}
+                  handleDelete={() => handleDelete(calligraphy.id)}
+                />
               </div>
             ))}
           </div>
