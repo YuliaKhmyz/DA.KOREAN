18d0e85993f5eb2a53c05aca6d5ba6ebdbb711ec
EvgeniaMarch <evgenia.m73@gmail.com>
2023-05-23 09:30:54 +0300

    course component page

--- client/src/features/courses/CoursesPage.tsx
+++ client/src/features/courses/CoursesPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectCourses } from './selectors';
+import CoursePageItem from './CoursePageItem';
+import AddCourseForm from './AddCourseForm';
 
 function CoursesPage(): JSX.Element {
-  return <div></div>;
+  const courses = useSelector(selectCourses);
+  console.log(courses);
+
+  return (
+    <div>
+      <div>
+        {courses.map((course) => (
+          <div key={course.id}>
+            <CoursePageItem course={course} />
+          </div>
+        ))}
+      </div>
+      <AddCourseForm />
+    </div>
+  );
 }
 
 export default CoursesPage;
