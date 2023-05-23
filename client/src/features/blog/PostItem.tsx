10bdf47be2a0c5ccf9a531ff7142c9df4893dfdf
Yulia Khmyz <xmyza@mail.ru>
2023-05-23 16:57:19 +0300

    import pics

--- client/src/features/blog/PostItem.tsx
+++ client/src/features/blog/PostItem.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { Post, PostId } from './types/Post';
+import pic1 from './pic1.jpg';
 
 function PostItem({ post }: { post: Post }): JSX.Element {
-  console.log('link', post.photo);
-
   return (
     <div>
       <h4>{post.title}</h4>
       <p>{post.description}</p>
       <img src={post.photo} alt="pic" />
-      <img
-        src="https://drive.google.com/file/d/13RdHkhgk4J661SnB1j9yaYPoe7kxMPL1/view"
-        alt="1"
-      />
+      <img src={pic1} alt="1" />
+      <img src="/images/pic1.jpg" alt="" />
     </div>
   );
 }
