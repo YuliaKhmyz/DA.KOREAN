34cc5e96acd2fa10a8c922e3eb4a3b6545e9c1b5
EvgeniaMarch <evgenia.m73@gmail.com>
2023-05-23 10:02:41 +0300

    start addform

--- client/src/features/courses/AddCourseForm.tsx
+++ client/src/features/courses/AddCourseForm.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-function AddCourseForm() {
-  return <div></div>;
+function AddCourseForm(): JSX.Element {
+  return (
+    <div>
+      <form>
+        <input placeholder="главное название" />
+        <input placeholder="главное описание" />
+        <input placeholder="главное название" />
+        <input placeholder="главное название" />
+        <input placeholder="главное название" />
+        <input placeholder="главное название" />
+        <input placeholder="главное название" />
+      </form>
+    </div>
+  );
 }
 
 export default AddCourseForm;
