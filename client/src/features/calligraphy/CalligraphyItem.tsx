78a1eac641ba4290686351eeb0916658d84eeef3
Yulia Khmyz <xmyza@mail.ru>
2023-05-25 14:15:18 +0300

    bugs fixed

--- client/src/features/calligraphy/CalligraphyItem.tsx
+++ client/src/features/calligraphy/CalligraphyItem.tsx
@@ -11,34 +11,62 @@ import koreanSpringBg from './korean_spring_bg.jpg';
 import koreanSummerBg from './korean_summer_bg.jpg';
 import koreanAutumnBg from './korean_autumn_bg.jpg';
 import koreanWinterBg from './korean_winter_bg.jpg';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../auth/selectors';
 
-function CalligraphyItem({ calligraphy, handleBuyCalligraphy }: { calligraphy: Calligraphy; handleBuyCalligraphy: (id: CalligraphyId) => void }): JSX.Element {
+function CalligraphyItem({
+  calligraphy,
+  handleBuyCalligraphy,
+}: {
+  calligraphy: Calligraphy;
+  handleBuyCalligraphy: (id: CalligraphyId) => void;
+}): JSX.Element {
   const navigate = useNavigate();
+  const user = useSelector(selectUser);
 
-  const images = [koreanSpringBg, koreanSummerBg, koreanAutumnBg, koreanWinterBg];
+  const images = [
+    koreanSpringBg,
+    koreanSummerBg,
+    koreanAutumnBg,
+    koreanWinterBg,
+  ];
 
   return (
     <>
       <div className="calligraphy-image-wrapper">
-        <img src={images[calligraphy.id - 1]} alt="осень в Корее" className="calligraphy-bg-image" />
-        <div className="calligraphy-corean-title">{calligraphy.koreantitle}</div>
+        <img
+          src={images[calligraphy.id - 1]}
+          alt="осень в Корее"
+          className="calligraphy-bg-image"
+        />
+        <div className="calligraphy-corean-title">
+          {calligraphy.koreantitle}
+        </div>
       </div>
       <div className="calligraphy-description-wrapper">
         <div className="calligraphy-item-title">{calligraphy.title}</div>
-        <p>Я разработала анти-стресс прописи для изучения корейского письма. Каждую из них можно приобрести отдельно или все вместе со скидкой. Я разработала анти-стресс прописи для изучения корейского письма. Каждую из них можно приобрести отдельно или все вместе со скидкой.</p>
+        <p>
+          Я разработала анти-стресс прописи для изучения корейского письма.
+          Каждую из них можно приобрести отдельно или все вместе со скидкой. Я
+          разработала анти-стресс прописи для изучения корейского письма. Каждую
+          из них можно приобрести отдельно или все вместе со скидкой.
+        </p>
 
-        <Link to="#">
-          <Button
-            variant="outline-secondary"
-            className="buy-btn"
-            onClick={() => {
+        <Button
+          variant="outline-secondary"
+          className="buy-btn"
+          onClick={() => {
+            if (!user) {
+              navigate('/auth/login');
+            }
+            if (user) {
               handleBuyCalligraphy(calligraphy.id);
               navigate('/mypage');
-            }}
-          >
-            Купить
-          </Button>
-        </Link>
+            }
+          }}
+        >
+          Купить
+        </Button>
       </div>
     </>
   );
